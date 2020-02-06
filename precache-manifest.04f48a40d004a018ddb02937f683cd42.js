176a8fd5f649fc5ad4fe74483961fd488c6904d4
EJ-C <10ngweekendofficial@gmail.com>
2020-02-06 00:43:24 -0500

    Updates

---
+++ precache-manifest.04f48a40d004a018ddb02937f683cd42.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1408fa67a371812322c4c34cf77ac48",
+    "url": "/worldblist/index.html"
+  },
+  {
+    "revision": "4cf6950520990d505ade",
+    "url": "/worldblist/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fc70276db2fe1c5b6c4d",
+    "url": "/worldblist/static/css/main.46390604.chunk.css"
+  },
+  {
+    "revision": "4cf6950520990d505ade",
+    "url": "/worldblist/static/js/2.77c6424e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/worldblist/static/js/2.77c6424e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc70276db2fe1c5b6c4d",
+    "url": "/worldblist/static/js/main.b29ad4df.chunk.js"
+  },
+  {
+    "revision": "b1078cf22d3d162a458b",
+    "url": "/worldblist/static/js/runtime-main.b51b0edd.js"
+  },
+  {
+    "revision": "c30ae2b344384318b9f418c45be2ed1a",
+    "url": "/worldblist/static/media/Sunflower-Medium.c30ae2b3.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/worldblist/static/media/logo.5d5d9eef.svg"
+  }
+]);
