3b9dc91ba439de43fa54bcf39007ad8436a6645b
EJ-C <10ngweekendofficial@gmail.com>
2020-02-27 15:21:08 -0500

    moved App.test.js file

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -7,40 +7,38 @@ import "../containers/App.css";
 
 class Brands extends Component {
   constructor() {
-    super();
-    this.state = {
-      brandsAll: [
-        {
-          id: 1,
-          name: "Barbour"
-        },
-        {
-          id: 2,
-          name: "A-COLD-WALL*"
-        },
-        {
-          id: 3,
-          name: 'A.P.C.'
-        }
-      ]
-    };
-  }
-  
-  
+                  super();
+                  this.state = {
+                    brandsAll: [
+                      {
+                        id: 1,
+                        name: "Barbour"
+                      },
+                      {
+                        id: 2,
+                        name: "A-COLD-WALL*"
+                      },
+                      {
+                        id: 3,
+                        name: "A.P.C."
+                      }
+                    ]
+                  };
+                }
 
   render() {
-    return   (
-          <div class="hide-child">
-            Brand
-            <Scroll>
-              <p className="branddropdown" class="child">
-                  {this.state.brandsAll.map(brandsAll =>  (
-                    <p key={brandsAll.id}> {brandsAll.name}</p>
-                  ))}
-              </p>
-            </Scroll>
-          </div>
-      );
+    return (
+      <div class="hide-child">
+        Brand
+        <Scroll>
+          <p className="branddropdown" class="child">
+            {this.state.brandsAll.map(brandsAll => (
+              <p key={brandsAll.id}> {brandsAll.name}</p>
+            ))}
+          </p>
+        </Scroll>
+      </div>
+    );
   }
 }
 
