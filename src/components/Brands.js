46accc60769afa9abeb39a778e79a5cc41dea4ae
EJ-C <10ngweekendofficial@gmail.com>
2020-02-27 13:44:51 -0500

    fixed brands array

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,77 +1,41 @@
 
-import React from "react";
+import React, { Component } from "react";
 import Scroll from "./Scroll";
-
 
 import "../containers/App.css";
 
-const Brands = ({ name, id }) => {
-  return (
+
+class Brands extends Component {
+  constructor() {
+    super();
+    this.state = {
+      brandsAll: [
+        {
+          id: 1,
+          name: 'A.P.C.'
+        },
+        {
+          id: 2,
+          name: 'A-COLD-WALL*'
+        }
+      ]
+    }
+  }
+
+  render () {
+    return(
     <div class="hide-child">
       Brand
       <Scroll>
         <div className="branddropdown" class="child">
-          <div>
-            <div> A-COLD-WALL* </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-            <div> A.P.C. </div>
-          </div>
+         {this.state.brandsAll.map(brandsAll => (
+           <div key={brandsAll.id}> {brandsAll.name}</div>
+         ))}
         </div>
       </Scroll>
     </div>
-  );
-};
+   )
+  }
+}
 
 export default Brands;
